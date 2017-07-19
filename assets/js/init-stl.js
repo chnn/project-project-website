09bb4e42dc9e22146084fd8f9beb4c745ff08bd3
Christopher Henn <chris@chrishenn.net>
2017-07-19 11:02:17 -0700

    Lint and tweak init-stl.js style

--- assets/js/init-stl.js
+++ assets/js/init-stl.js
@@ -1,59 +1,58 @@
-document.querySelectorAll('.stl-container').forEach(function(el) {
-	initSTLContainer(el);
+/* global THREE */
+
+document.querySelectorAll(".stl-container").forEach(function(el) {
+  initSTLContainer(el);
 });
 
 function initSTLContainer(container) {
-	var scene, camera, cameraControls, renderer;
+  var scene = new THREE.Scene();
+  var renderer = new THREE.WebGLRenderer({ antialias: true });
+  var camera = new THREE.PerspectiveCamera(75, container.offsetWidth / container.offsetHeight, 0.1, 1000);
+  var cameraControls = new THREE.OrbitControls(camera, renderer.domElement);
 
-	function loadSTL(url) {
-		var loader = new THREE.STLLoader();
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(container.offsetWidth, container.offsetHeight);
+  renderer.setClearColor("white");
 
-		loader.load(url, function(geometry) {
-			var material = new THREE.MeshLambertMaterial({ color: '#2980b9' });
-			var mesh = new THREE.Mesh(geometry, material);
+  camera.position.z = 1.5;
+  cameraControls.target.set(0, 0, 0);
+  cameraControls.addEventListener("change", render);
 
-			geometry.computeFaceNormals();
-			geometry.computeVertexNormals();
+  var directionalLight = new THREE.DirectionalLight(0xffffff, 1.35);
 
-			mesh.scale.set(0.013, 0.013, 0.013);
+  directionalLight.position.set(1, 1, 1);
+  scene.add(directionalLight);
+  scene.add(new THREE.AmbientLight("#7f8c8d"));
 
-			var box = new THREE.Box3().setFromObject(mesh);
-			var pos = (new THREE.Vector3()).addVectors(box.min, box.max).divideScalar(2);
+  function addGeometryToScene(geometry) {
+    var material = new THREE.MeshLambertMaterial({ color: "#2980b9" });
+    var mesh = new THREE.Mesh(geometry, material);
 
-			mesh.position.set(-pos.x, -pos.y, -pos.z);
+    geometry.computeFaceNormals();
+    geometry.computeVertexNormals();
 
-			scene.add(mesh);
-			container.className += " loaded";
+    mesh.scale.set(0.013, 0.013, 0.013);
 
-			render();
-		});
-	}
+    var box = new THREE.Box3().setFromObject(mesh);
+    var pos = (new THREE.Vector3()).addVectors(box.min, box.max).divideScalar(2);
 
-	function render() {
-		renderer.render(scene, camera);
-	}
+    mesh.position.set(-pos.x, -pos.y, -pos.z);
 
-	renderer = new THREE.WebGLRenderer({ antialias: true });
-	scene = new THREE.Scene();
-	camera = new THREE.PerspectiveCamera(75, container.offsetWidth / container.offsetHeight, 0.1, 1000);
-	cameraControls = new THREE.OrbitControls(camera, renderer.domElement);
+    scene.add(mesh);
+    container.className += " loaded";
 
-	container.appendChild(renderer.domElement);
-	renderer.setPixelRatio(window.devicePixelRatio)
-	renderer.setSize(container.offsetWidth, container.offsetHeight);
-	renderer.setClearColor('white');
+    render();
+  }
 
-	camera.position.z = 1.5;
-	cameraControls.target.set(0, 0, 0);
-	cameraControls.addEventListener('change', render);
+  function render() {
+    renderer.render(scene, camera);
+  }
 
-	scene.add(new THREE.AmbientLight('#7f8c8d'));
+  var filename = container.dataset.filename.trim();
+  var loader = new THREE.STLLoader();
 
-	var directionalLight = new THREE.DirectionalLight(0xffffff, 1.35);
+  loader.load(filename, addGeometryToScene);
 
-	directionalLight.position.set(1, 1, 1);
-	scene.add(directionalLight);
-
-	loadSTL(container.dataset.filename);
-	render();
+  render();
+  container.appendChild(renderer.domElement);
 }
